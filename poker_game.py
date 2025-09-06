e42df13ed056915891654140038950c63b399567
Lumos <artlis321@gmail.com>
2025-09-06 15:49:31 -0700

    working on run_hand

--- poker_game.py
+++ poker_game.py
@@ -1,6 +1,7 @@
 from itertools import combinations
+import random
 
-deck = [(i,j) for i in range(2,15) for j in ["H","D","C","S"]]
+base_deck = [(i,j) for i in range(2,15) for j in ["H","D","C","S"]]
 
 class PokerGame:
     def __init__(self,player_classes,starting_pot):
@@ -9,10 +10,10 @@ class PokerGame:
         self.banks = [starting_pot for i in range(self.num_players)]
 
     def hand_rank(hand):
-        is_flush = all([card[i][1] == hand[0][1] for card in hand])
+        is_flush = all([card[1] == hand[0][1] for card in hand])
 
         sorted_hand = sorted(hand)
-        numerical_hand = [card[i] for card in sorted_hand]
+        numerical_hand = [card[0] for card in sorted_hand]
 
         lowest_card = min(numerical_hand)
         is_straight = (numerical_hand == [2,3,4,5,14]) or ([val-lowest_card for val in numerical_hand] == [0,1,2,3,4])
@@ -68,11 +69,14 @@ class PokerGame:
         best_rank = max(hand_ranks)
         return [i for i in range(len(hand_ranks)) if hand_ranks[i]==best_rank]
 
-    def run_hand(self,dealer_index):
+    def run_hand(self,small_index):
         """
         update_dict = {
-            cur_phase : ["Preflop","Flop","Turn","River","Reveal"],
-            your_position : [0(small blind),...,num_players-1(dealer)],
+            cur_phase : in ["Preflop","Flop","Turn","River","Reveal"],
+            your_position : in [0(small blind),...,num_players-1(dealer)],
+            cur_actor : in [0,...,num_players-1],
+            last_actor : in [0,...,num_players-1]
+            folded_players : subset [0,...,num_players-1]
             your_hand : [(val,suit),(val,suit)],
             all_hands: [[(val,suit),(val,suit)],...] # this value is None unless in Reveal
             public_hand : [(val,suit),...],
@@ -80,9 +84,67 @@ class PokerGame:
             required_bid : ###,
             cur_bids : [###,###,###,###],
             your_bank : ###,
-            all_bank : [###,###,###,###]
+            all_banks : [###,###,###,###]
         }
         """
+
+        position_to_player_index = [i % self.num_players for i in range(small_index,small_index+self.num_players)]
+        cur_deck = random.shuffle(base_deck)
+        hands = [[cur_deck.pop(),cur_deck.pop()] for _ in range(self.num_players)]
+
+        cur_phase = "Preflop"
+        cur_actor = 0
+        last_actor = 0
+        folded_players = []
+        all_hands = None
+        public_hand = []
+        pots = [(0,[i for i in range(self.num_players)])]
+        required_bid = 2
+        cur_bids = [0 for _ in range(self.num_players)]
+        all_banks = self.banks
+
+        def send_updates():
+            for i in range(self.num_players):
+                player_index = position_to_player_index[i]
+                update_dict = {
+                    "cur_phase" : cur_phase,
+                    "cur_actor" : cur_actor,
+                    "last_actor" : last_actor,
+                    "folded_players" : folded_players,
+                    "your_hand" : hands[i],
+                    "all_hands" : all_hands,
+                    "public_hand" : public_hand,
+                    "pots" : pots,
+                    "required_bit" : required_bid,
+                    "cur_bids" : cur_bids,
+                    "your_bank" : self.banks[player_index],
+                    "all_banks" : all_banks
+                }
+                self.players[player_index].receive_game_update(update_dict)
+
+        ### Preflop
+        self.banks[position_to_player_index[0]] -= 1
+        cur_bids[0] = 1
+        self.banks[position_to_player_index[1]] -= 2
+        cur_bids[1] = 2
+        cur_actor = 2 % self.num_players
+        last_actor = 1
+
+        send_updates()
+
+        while cur_actor <= last_actor:
+            if cur_actor in folded_players:
+                cur_actor += 1
+            else:
+                actor_index = position_to_player_index[cur_actor]
+                next_play = self.players[actor_index].next_play()
+
+                if next_play[0] == "Fold":
+                    folded_players.append(cur_actor)
+                    folded_players = sorted(folded_players)
+                if next_play[0] == "Check":
+                    #####
+
 
 # to be extended by players
 class PokerBot:
@@ -90,7 +152,7 @@ class PokerBot:
         self.num_players = num_players
         self.my_index = my_index
 
-    def receive_game_update(self):
+    def receive_game_update(self,update_dict):
         return
 
     def next_play(self):
@@ -99,5 +161,6 @@ class PokerBot:
         return ["Check"] # automatically folds if cannot check
         return ["Call"] # automatically raises to required_bid
         return ["Raise",###] # raises below required_bid automatically raise to it, raises above your_bank automatically go all in
+        an error automatically folds
         """
         return ["Fold"]
